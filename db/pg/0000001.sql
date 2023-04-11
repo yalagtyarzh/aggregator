611b81c8dfb5f7f5b088d55319a5f00a09768fa3
Dmitry Shtanko <shtanko.dmitriy@wb.ru>
2023-04-11 21:29:49 +0300

    dev: +

--- db/pg/0000001.sql
+++ db/pg/0000001.sql
@@ -1,6 +1,73 @@
 -- +migrate Up
 
+CREATE TABLE rating
+(
+    id       SERIAL PRIMARY KEY NOT NULL,
+    name     VARCHAR(2)         NOT NULL,
+    name_ext VARCHAR(25)        NOT NULL,
+);
 
+CREATE TABLE products
+(
+    id           SERIAL PRIMARY KEY NOT NULL,
+    title        TEXT               NOT NULL,
+    description  TEXT               NOT NULL,
+    year         INTEGER            NOT NULL,
+    release_date TIMESTAMP          NOT NULL,
+    studio       TEXT               NOT NULL,
+    rating_id    INTEGER            NOT NULL REFERENCES rating (id)
+);
+
+CREATE TABLE roles
+(
+    name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE permissions
+(
+    name text NOT NULL UNIQUE
+);
+
+CREATE TABLE roles_permissions
+(
+    role        TEXT NOT NULL REFERENCES roles (name),
+    permissions TEXT NOT NULL REFERENCES roles (permissions),
+    UNIQUE (role, permissions)
+);
+
+CREATE TABLE users
+(
+    id         UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    first_name TEXT             NOT NULL,
+    last_name  TEXT             NOT NULL,
+    user_name  TEXT             NOT NULL UNIQUE,
+    email      TEXT             NOT NULL UNIQUE,
+    password   TEXT             NOT NULL,
+    role       TEXT             NOT NULL REFERENCES roles (name)
+);
+
+CREATE TABLE reviews
+(
+    id           UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    score        INTEGER          NOT NULL,
+    content      TEXT             NOT NULL,
+    content_html TEXT             NOT NULL,
+    CHECK (score BETWEEN 0 AND 100)
+);
 
 -- +migrate Down
+
+DROP TABLE reviews;
+
+DROP TABLE users;
+
+DROP TABLE roles_permissions;
+
+DROP TABLE permissions;
+
+DROP TABLE roles;
+
+DROP TABLE products;
+
+DROP TABLE rating
 
